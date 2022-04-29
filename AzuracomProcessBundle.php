03128dc091c4e0c1b5897f4f60bc61cab908d123
Thibaut <tdumalin@azuracom.com>
2022-04-29 08:43:15 +0200

    AzuracomProcessBundle compability fix

--- AzuracomProcessBundle.php
+++ AzuracomProcessBundle.php
@@ -10,7 +10,10 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 
 class AzuracomProcessBundle extends AbstractResourceBundle
 {
-    protected $mappingFormat = ResourceBundleInterface::MAPPING_YAML;
+    public function __construct()
+    {
+        $this->mappingFormat = ResourceBundleInterface::MAPPING_YAML;
+    }
     
     public function getSupportedDrivers(): array
     {
